7c3cea081556a47455f66cd4bce2eaebf2429318
Micah <micahshegbo1@gmail.com>
2026-08-20 15:07:04 +0100

    More sructured colors added

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -6,11 +6,14 @@ export default function Home() {
 
       {/* clean this div line 8-16*/}
       <div className="Testing mt-50">
+        <div className="">THE L</div>
             All below is a test
-        <p className="font-title-small text-primary">dsfsdfsfsdfdsfsd</p>
+        <p className="font-title-small">dsfsdfsfsdfdsfsd</p>
         <p className="text-base text-dark-muted">therhehhehf</p>
-        <h1>SADRTRTDRHTDRHTHY
-          GTFTF</h1>
+        <h3>SADRTRTDRHTDRHTHY
+          GTFTF</h3>
+
+
         <button className="bg-primary text-light">fwefewfwefwef</button>
 
       </div>
