e67055cde25a5f5e2417b10cf44fde6bd32f7e9c
Micah <micahshegbo1@gmail.com>
2026-08-20 02:38:33 +0100

    Structured colors, important font, and updated page layouts

---
+++ app/(public)/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* Top navbar */}
+            <div className="bg-dark py-4">
+                <div className="max-w-[80%] mx-auto">
+                    topnav content here
+                </div>
+            </div>
+            
+
+            {/* Main Navbar */}
+            <div className="">
+                 <div className="max-w-[80%] mx-auto">
+                    Main nav content here
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
+import Navbar from "./components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     //   <body className="min-h-full flex flex-col">{children}</body>
     // </html>
     <div className="min-h-full flex flex-col">
+      <Navbar />
       {children}
     </div>
   );
