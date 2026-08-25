ba1e8a71684f7eb595ea0aa1df1d7cf1db277307
Micah <micahshegbo1@gmail.com>
2026-08-25 15:24:02 +0000

    cleaned commented codes in all layouts

--- app/(public)/home/page.tsx
+++ app/(public)/home/page.tsx
@@ -1,24 +1,3 @@
-// import React from 'react'
-
-// import { BeforeFooterData } from '@/data'
-// import Navbar from "../components/Navbar"
-// import Footer from "../components/Footer"
-// import BeforeFooter from "../components/BeforeFooter"
-// import Hero from "../components/Hero"
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Hero />
-//       <BeforeFooter BeforeFooterData={BeforeFooterData}/>
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default Home
-
 import React from 'react'
 
 const HomePage = () => {
