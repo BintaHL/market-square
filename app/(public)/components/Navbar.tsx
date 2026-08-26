3cf150a7a1ebf06d8ddcaa97ba007e976b316b8f
Binta Lawrence <bintah.lawrence@gmail.com>
2026-08-26 20:35:33 +0100

    Update on responsiveness

--- app/(public)/components/Navbar.tsx
+++ app/(public)/components/Navbar.tsx
@@ -7,7 +7,7 @@ import { Search } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full left-0 top-0'>
+    <div className='fixed w-full left-0 top-0 z-100'>
         <div className='z-100 h-[140px] shadow-md'>
             {/* First part of the nav */}
             <div className='w-full h-[60px] md:h-[40px] bg-[#000000] py-3'>
@@ -28,17 +28,17 @@ const Navbar = () => {
                 </div>
             </div>
             {/* The second part of the nav */}
-            <div className='w-full h-[100px] z-100'>
+            <div className='w-full z-100'>
                 <div className='container mx-auto flex flex-col md:flex-row items-center py-5 md:py-8 gap-20 justify-between'>
                     <div className='font-bold'>
                         <h3>CARTS</h3>
                     </div>
                     <div className='flex flex-col md:flex-row justify-between md:gap-40 bg-white'>
                         <div className='flex flex-col md:flex-row mx-auto'>
-                            <Link href="/" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[18px]'>Home</Link>
-                            <Link href="/contact" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[18px]'>Contact</Link>
-                            <Link href="/about" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[18px]'>About</Link>
-                            <Link href="/auth/signin" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[18px] whitespace-nowrap'>Sign Up</Link>
+                            <Link href="/" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[12px]'>Home</Link>
+                            <Link href="/contact" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[12px]'>Contact</Link>
+                            <Link href="/about" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[12px]'>About</Link>
+                            <Link href="/auth/signin" className='mb-5 md:pr-6 hover:underline hover:decoration-mist-400 hover:decoration-2 hover:underline-offset-4 cursor-pointer text-2xl md:text-[12px] whitespace-nowrap'>Sign Up</Link>
                         </div>
                         <div className='flex flex-col md:flex-row gap-4 items-center justify-center'>
                             <div className="relative flex items-center justify-center">
