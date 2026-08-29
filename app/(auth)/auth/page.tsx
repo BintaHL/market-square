1aec61355a9f666a74cd08c07d561d6cff689302
Micah <micahshegbo1@gmail.com>
2026-08-29 09:29:09 +0000

    Removed part of the reauable addtocart button and custom product cart

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,13 +1,30 @@
+import axios from "axios"
 
+interface Userdata {
+  id: number,
+  full_name: string,
+  username: string,
+  email: string,
+  phone_number: string,
+  role: string,
+}
 
-const AuthHome = () => {
+const AuthHome = async() => {
+  const res = await axios.get<Userdata>(`https://opt-evacuate-abrasive.ngrok-free.dev/users/me`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  const data = res.data
+  console.log(data)
+
   return (
     <div>
       
       AuthHome
       
       <div className="">
-        Content here
+        
       </div>
         
     </div>
